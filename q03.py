f34867b7658f07e032379845ed926ab32597655a
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:38:18 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,19 @@
 ## D,23
 ## E,67
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ',') for line in data]
+data = [line.replace(':', ',') for line in data]
+data = [line.replace(' ', ',') for line in data]
+data = [row.split(',') for row in data]
+terminos = [[row[0]] for row in data]
+terminos = ','.join([','.join(line) for line in terminos]).split(',')
+term = set(sorted(terminos))
+for t in sorted(term):
+    suma=0
+    for row in data:
+        if row[0] == t:
+            suma = suma + int(row[1])
+    print(t + "," + str(suma))
+    print(t + "," + str(suma))
