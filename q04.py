2500be164cd50cb9c040ef3b2507266dfddd5567
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:41:41 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -14,3 +14,16 @@
 ## 11,2
 ## 12,3
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ',') for line in data]
+data = [line.replace(':', ',') for line in data]
+data = [line.replace(' ', ',') for line in data]
+data = [line.replace('-', ',') for line in data]
+data = [row.split(',') for row in data]
+meses = [[row[3]] for row in data]
+meses = ','.join([','.join(line) for line in meses]).split(',')
+term = set(sorted(meses))
+for t in sorted(term):
+    aux = meses.count(t)
+    print(t+","+str(aux))
