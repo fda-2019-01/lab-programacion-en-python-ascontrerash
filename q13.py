74cb90f91b6205aa7e31e0c06e42bf7fc636e1dd
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:46:25 -0500

    Update q13.py

--- q13.py
+++ q13.py
@@ -10,3 +10,15 @@
 ## E,11
 ## E,16
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ';') for line in data]
+data = [row.split(';') for row in data]
+data = [[row[0],row[4]] for row in data]
+for row in data:
+    suma=0
+    string = row[1]
+    for i in range(len(string)):
+        if string[i-1]==":":
+            suma+=int(string[i])
+    print(row[0]+","+str(suma))
