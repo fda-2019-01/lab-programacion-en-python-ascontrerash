925f8f7d632d98d260679fd2234ec62d723c0d84
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:45:57 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -10,3 +10,20 @@
 ## f,110
 ## g,35
 ##
+import re
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ';') for line in data]
+data = [row.split(';') for row in data]
+data = [[row[1],row[3]] for row in data]
+terminos = [[row[1]] for row in data]
+terminos = ','.join([','.join(line) for line in terminos]).split(',')
+term = sorted(set(terminos))
+term
+data
+for t in term:
+    aux=0
+    for row in data:
+        if re.search(t, row[1]):
+            aux+=int(row[0])
+    print(t+","+str(aux))
