2826c7c51e4e8f652c697648c73e66108fe6109b
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 01:54:38 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -13,3 +13,24 @@
 ## iii,2,7
 ## jjj,2,5
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ';') for line in data]
+data = [row.split(';') for row in data]
+data = [[row[4]] for row in data]
+data = ','.join([','.join(line) for line in data]).split(',')
+#data = [line[:-2] for line in data]
+terminos = [line[:-2] for line in data]
+term = sorted(set(terminos))
+data = [line.replace(':',';') for line in data]
+data = [row.split(';') for row in data]
+for t in term:
+    mini=999999
+    maxi=0
+    for row in data:
+        if t == row[0]:
+            if int(row[1])>maxi:
+                maxi=int(row[1])
+            elif int(row[1])<mini:
+                mini=int(row[1])
+    print(t+","+str(mini)+","+str(maxi))
