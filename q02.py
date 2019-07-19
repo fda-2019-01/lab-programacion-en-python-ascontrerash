6390fb050b61966fa51b81744e27a56b3996ed23
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:37:39 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,14 @@
 ## D,6
 ## E,14
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ',') for line in data]
+data = [line.replace(':', ',') for line in data]
+data = [line.replace(' ', ',') for line in data]
+terminos = [[row[0]] for row in data]
+terminos = ','.join([','.join(line) for line in terminos]).split(',')
+term = set(sorted(terminos))
+for t in sorted(term):
+    aux = terminos.count(t)
+    print(t+","+str(aux))
