580e4616580f06232fec0aea541b500ec427f474
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:44:48 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -15,3 +15,17 @@
 ## jjj,18
 ##
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ';') for line in data]
+data = [row.split(';') for row in data]
+data = [[row[4]] for row in data]
+data = ','.join([','.join(line) for line in data]).split(',')
+data = [line[:-2] for line in data]
+term = sorted(set(data))
+for t in term:
+    suma=0
+    for row in data:
+        if row == t:
+            suma+=1
+    print(t+","+str(suma))
