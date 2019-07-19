e833dde994cad3207cf0d6aac70637f66353f54e
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:37:08 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,13 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ',') for line in data]
+data = [line.replace(':', ',') for line in data]
+data = [line.replace(' ', ',') for line in data]
+data = [row.split(',') for row in data]
+suma=0
+for row in data:
+    suma = suma + int(row[1])
+print(suma)
