2759e62d5817e9403de63a45e5fbf62cb080f969
Alex Santiago Contreras Hernández <50633910+ascontrerash@users.noreply.github.com>
2019-07-19 00:45:20 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -11,3 +11,13 @@
 ## E,2,3
 ## E,3,3
 ##
+import math
+data = open('data.csv', 'r').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace('\t', ';') for line in data]
+data = [row.split(';') for row in data]
+data = [[row[0],row[3],row[4]] for row in data]
+for row in data:
+    aux1=math.ceil((len(row[1]))/2)
+    aux2=math.ceil((len(row[2]))/6)
+    print(row[0]+","+str(aux1)+","+str(aux2))
